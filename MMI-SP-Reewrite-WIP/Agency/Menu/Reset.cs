fa9d2dc3fa1220a45001becb9af2b69615ce1fb6
JesusRondon2310 <126926654+JesusRondon2310@users.noreply.github.com>
2026-05-10 20:11:35 +0200

    Add files via upload

---
+++ MMI-SP-Reewrite-WIP/Agency/Menu/Reset.cs
@@ -0,0 +1,13 @@
+﻿using NativeUI;
+
+namespace MMI_SP.Agency.Menu
+{
+    internal static class Execute
+    {
+        internal static void Rebuild(UIMenu mainMenu)
+        {
+            mainMenu.Clear();
+            Menu.InsureButtonBuild(mainMenu);
+        }
+    }
+}
