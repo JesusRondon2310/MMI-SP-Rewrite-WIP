9618b45c8696a9bc3e5438b7bf1d6e8bf9231163
JesusRondon2310 <126926654+JesusRondon2310@users.noreply.github.com>
2026-05-18 00:33:52 +0200

    Add files via upload

---
+++ MMI-SP-Rewrite-WIP/Insurance/Observer/Delivery/EntityPositon.cs
@@ -0,0 +1,16 @@
+﻿using GTA.Math;
+
+namespace MMI_SP.Insurance.Observer.Delivery
+{
+    internal struct EntityPosition
+    {
+        internal Vector3 Position;
+        internal float Heading;
+
+        internal EntityPosition(Vector3 pos, float heading)
+        {
+            Position = pos;
+            Heading = heading;
+        }
+    }
+}
