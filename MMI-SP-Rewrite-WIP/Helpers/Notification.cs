ded25d60106cbb191756a89daaf71c6797f244e6
JesusRondon2310 <126926654+JesusRondon2310@users.noreply.github.com>
2026-05-22 18:34:59 +0200

    Add files via upload

---
+++ MMI-SP-Rewrite-WIP/Helpers/Notification.cs
@@ -0,0 +1,14 @@
+﻿namespace MMI_SP.Helpers
+{
+    internal static class Notification
+    {
+        public static void Show(string title, string message, string description)
+        {
+            string text = string.IsNullOrEmpty(description)
+                ? $"{title}: {message}"
+                : $"{title}: {message} {description}";
+
+            GTA.UI.Notification.PostTicker(text, isImportant: false);
+        }
+    }
+}
